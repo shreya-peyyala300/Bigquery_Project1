cb8814d870219ce1dd995bfa137c86c0e6a9e836
shreya-peyyala300 <saipeyyala2008@gmail.com>
2025-03-29 18:35:44 +0530

    Data Export

---
+++ pandas-mysql-export.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from mysql.connector import errorcode
+import configparser
+import pandas as pd
+import os
+
+cur_path=os.getcwd()
+file= 'House_price.csv'
+file_path=os.path.join(cur_path,'data_files', file)
+print(file_path)
+
+# Load configuration file using configparser
+config = configparser.ConfigParser()
+config.read(r"C:\Users\saipe\OneDrive\Desktop\bigquery-project1\config.ini")
+
+# Extract MySQL connection parameters from the config file
+db_config = {
+    'user': config['mysql']['user'],
+    'password': config['mysql']['password'],
+    'host': config['mysql']['host'],
+    'database': config['mysql']['database']
+}
+
+# Connect to the MySQL database
+conn = mysql.connector.connect(**db_config)
+
+
+# SQL query (using triple quotes for better readability)
+query = """
+    SELECT year, id, city_name
+    FROM oscar_sql.city_house_price
+    where year between 2004 and 2025;
+"""
+
+df=pd.read_sql(query,conn)
+
+yr_2005=df['year']==2005
+
+df.to_csv(file_path)
+
+
+# # Close the connection
+# cursor.close()
+conn.close()
