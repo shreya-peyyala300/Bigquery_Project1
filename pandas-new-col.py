30a63cd3d7c08e557b047af74770049e4aa7fd02
shreya-peyyala300 <saipeyyala2008@gmail.com>
2025-03-29 19:18:17 +0530

    add new col

--- pandas-new-col.py
+++ pandas-new-col.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 
 cur_path=os.getcwd()
-file= 'House_price.csv'
+file= 'House_price_tf.csv'
 file_path=os.path.join(cur_path,'data_files', file)
 print(file_path)
 
@@ -27,7 +27,11 @@ conn = mysql.connector.connect(**db_config)
 
 # SQL query (using triple quotes for better readability)
 query = """
-    SELECT year, id, city_name
+    SELECT year, id, city_name ,
+    CASE 
+    WHEN year % 2 = 0 THEN "even"
+    WHEN year % 2 != 0 THEN "odd"
+    END AS even_odd_year
     FROM oscar_sql.city_house_price
     where year between 2004 and 2025;
 """
@@ -36,8 +40,8 @@ df=pd.read_sql(query,conn)
 
 yr_2005=df['year']==2005
 
-df.to_csv(file_path)
-
+df.to_csv(file_path,index=False)
+# df['year'].to_csv(file_path,index=False)
 
 # # Close the connection
 # cursor.close()
